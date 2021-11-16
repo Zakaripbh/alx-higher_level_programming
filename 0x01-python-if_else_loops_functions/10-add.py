a059a98595710a0867f5c06899af0453588bcb07
zakaripbh <zakariusman4real@gmail.com>
2021-11-16 11:13:37 -0800

    0x01-python-if_else_loops_functions

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 10-add.py
+# Zakari Usman <zakariusman4real@gmail.com>
+
+
+def add(a, b):
+    """Return the addition of a and b."""
+    return (a + b)
---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 11-pow.py
+# Zakari Usman <zakariusman4realgmail.com>
+
+
+def pow(a, b):
+    """Return a to the power of b."""
+    return (a ** b)
