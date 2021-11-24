08559b3bb967287cbf17f7b300f25ee3f5543801
zakaripbh <zakariusman4real@gmail.com>
2021-11-24 11:37:25 -0800

    0x04-python-more_data_structures

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 3-common_elements.py
+# Zakari Usman <zakariusman4real@gmail.com>
+
+
+def common_elements(set_1, set_2):
+    """Return a set of common elements in two sets."""
+    return (set_1 & set_2)
---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 5-number_keys.py
+# Zakari Usman <zakariusman4real@gmail.com>
+
+
+def number_keys(a_dictionary):
+    """Return the number of keys in a dictionary."""
+    return (len(a_dictionary))
