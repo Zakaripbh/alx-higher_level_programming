7a1f94b3514e7ca48c364743f6debfe7828b164c
zakaripbh <zakariusman4real@gmail.com>
2021-12-06 13:23:54 -0800

    0x05-python-exceptions

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 5-raise_exception.py
+# Zakari Usman <zakariusman4real@gmail.com>
+
+
+def raise_exception():
+    """Raise a TypeError exception."""
+    raise TypeError
---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 6-raise_exception_msg.py
+# Zakari Usman <zakariusman4real@gmail.com>
+
+
+def raise_exception_msg(message=""):
+    """Raise a NameError exception with a message."""
+    raise NameError(message)
